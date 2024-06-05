6af5803aa94a6335b026602f6ac3260be39edbeb
Pedro Lucas <pl.lucassouza@gmail.com>
2024-06-05 10:26:57 -0300

    adding the jpa and postgree dependency and mapping the entities

--- src/main/java/br/com/alura/screenmatchSpring/model/Serie.java
+++ src/main/java/br/com/alura/screenmatchSpring/model/Serie.java
@@ -1,16 +1,25 @@
 package br.com.alura.screenmatchSpring.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "tbl_series")
 public class Serie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
 
+    @Column(name = "total_seasons")
     private int totalSeasons;
 
     private double rating;
 
+    @Enumerated(EnumType.STRING)
     private Category genre;
 
     private String actors;
@@ -27,5 +36,69 @@ public class Serie {
         this.actors = dataSeries.actors();
         this.plot = dataSeries.plot();
         this.poster = dataSeries.poster();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getTotalSeasons() {
+        return totalSeasons;
+    }
+
+    public void setTotalSeasons(int totalSeasons) {
+        this.totalSeasons = totalSeasons;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public Category getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Category genre) {
+        this.genre = genre;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
     }
 }
